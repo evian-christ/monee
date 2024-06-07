273c217dcbf14d59feae2d17f48ed357f2ab0af5
evian-christ <thelastorcas@gmail.com>
2024-06-07 15:53:14 +0100

    first basic tkinter test file. (kivy abandoned for now, due to it's unnecessary complexity for my app

--- tktest.py
+++ tktest.py
@@ -1,9 +1,17 @@
 from tkinter import *
 
+# main window
 main = Tk()
 
+# Title
 main.title("Testing...")
 
+# Size
 main.geometry("350x200")
 
+# Labels
+lbl = Label(main, "Welcome!")
+lbl.grid()
+
+# Execute TkInter
 main.mainloop()
