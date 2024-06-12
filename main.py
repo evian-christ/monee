5c700af9c8fdde750362750c53dd52393e52c4b3
evian-christ <thelastorcas@gmail.com>
2024-06-12 19:47:29 +0100

    adjusted size of buttons and positions

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ def clicked():
 
 # button
 btn_add = Button(main, text = "Add", style = 'main.TButton', padding=20, command = clicked)
-btn_view = Button(main, text = "View", style = 'main.TButton', padding=20, command = clicked)
-btn_settings = Button(main, text = "Settings", style = 'main.TButton', padding=20, command = clicked)
+btn_view = Button(main, text = "View", style = 'main.TButton', padding=10, command = clicked)
+btn_settings = Button(main, text = "Settings", style = 'main.TButton', padding=10, command = clicked)
 
 # set button grid
-btn_add.grid(column = 0, row = 2)
-btn_view.grid(column = 0, row = 3)
-btn_settings.grid(column = 0, row = 4)
+btn_add.grid(column = 0, row = 2, pady = 20)
+btn_view.grid(column = 0, row = 3, pady = 20)
+btn_settings.grid(column = 0, row = 4, pady = 20)
 
 # Execute TkInter
 main.mainloop()
