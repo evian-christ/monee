6b7b6066622d53edaaa7d1ec43b6354ccb1e0c09
evian-christ <thelastorcas@gmail.com>
2024-06-14 20:31:42 +0100

    continued from last commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter.ttk import *
 from ttkthemes import ThemedTk
 from dateAndTime import *
 
-# main window
+#----------------main window----------------
 main = ThemedTk(theme='yaru')
 main.grid_columnconfigure(0, weight=1)
 
@@ -10,28 +10,28 @@ main.grid_columnconfigure(0, weight=1)
 s = Style()
 s.configure('main.TButton', font = ('Consolas', 20))
 
-# Title
+#------------------Title--------------------
 main.title("monee v0.0.1")
-# Size
+#-------------------Size--------------------
 main.geometry("1080x720")
 
-# Date
+#-------------------Date--------------------
 tday = Label(main, text = today, font=('Consolas', 20), padding=20)
 tday.grid(pady = 70, sticky='n')
 
-# function
+#-----------------function------------------
 def clicked():
     pass
 
-# button
+#------------------button--------------------
 btn_add = Button(main, text = "Add", style = 'main.TButton', padding=20, command = clicked)
 btn_view = Button(main, text = "View", style = 'main.TButton', padding=10, command = clicked)
 btn_settings = Button(main, text = "Settings", style = 'main.TButton', padding=10, command = clicked)
 
-# set button grid
+#--------------set button grid---------------
 btn_add.grid(column = 0, row = 2, pady = 20)
 btn_view.grid(column = 0, row = 3, pady = 20)
 btn_settings.grid(column = 0, row = 4, pady = 20)
 
-# Execute TkInter
+#---------------Execute TkInter--------------
 main.mainloop()
