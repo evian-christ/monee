4bd91c0ac596c0ae5924de2d02bfdeb966c44d63
evian-christ <thelastorcas@gmail.com>
2024-06-17 16:23:57 +0100

    learned about inserting values into db (connect commit and variable insertion)

--- dbtest.py
+++ dbtest.py
@@ -1,4 +1,6 @@
 import sqlite3
+import datetime
+import time
 
 #-------------------DB----------------------
 db = sqlite3.connect('data.db')
@@ -6,8 +8,16 @@ db = sqlite3.connect('data.db')
 db.execute('''
 CREATE TABLE if not exists expenses
            (
-           id INTEGER,
+           id INTEGER PRIMARY KEY,
            date INTEGER,
            desc VARCHAR(255)
            )
 ''')
+
+
+date = datetime.date(2024, 6, 15)
+desc = "McDonalds"
+
+db.execute("INSERT INTO expenses (date, desc) VALUES (?, ?)",(time.mktime(date.timetuple()), desc)) 
+
+db.commit()
