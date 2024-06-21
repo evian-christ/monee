faafb99aa76f6384b5027aef3d61b1ef8fdf4954
evian-christ <thelastorcas@gmail.com>
2024-06-21 16:04:59 +0100

    the width of the treeview now depends on the width of the window

--- view.py
+++ view.py
@@ -1,8 +1,5 @@
 from tkinter import *
-from tkinter import messagebox
 from tkinter.ttk import *
-from dateAndTime import *
-
 import sqlite3
 
 def open_view():
@@ -10,24 +7,13 @@ def open_view():
 
     root.title("View your entries")
     root.geometry("1000x700+300+200")
-    
-    frame = Frame(root)
+
+    frame = Frame(root, padding=15)
 
     advbtn = Button(frame, text="Advanced")
     table = Treeview(frame, columns=
                      ("Date", "Name", "Category", "Cost", "Rating", "Description", "Remark"),
                      show="headings")
-
-    table.column("Date", width=100, stretch=NO, anchor=CENTER)
-    table.column("Name", width=100, stretch=NO, anchor=CENTER)
-    table.column("Category", width=100, stretch=NO)
-    table.column("Cost", width=100, stretch=NO)
-    table.column("Rating", width=100, stretch=NO)
-    table.column("Description", width=100, stretch=NO)
-    table.column("Remark", width=100, stretch=NO)
-
-    table.column("#0", stretch=NO)
-    table.column("#1", stretch=NO)
 
     table.heading("Date", text="Date")
     table.heading("Name", text="Name")
@@ -37,9 +23,12 @@ def open_view():
     table.heading("Description", text="Description")
     table.heading("Remark", text="Remark")
 
-    frame.grid(column=0, row=0)
+    frame.grid(column=0, row=0, sticky="nsew")  # Expand frame within root
+    root.columnconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=1)  # Expand column 0 of root
+    frame.columnconfigure(1, weight=1)  # Expand column 0 of root
 
     advbtn.grid(column=1, row=0, sticky='e')
-    table.grid(column=0, row=1, columnspan=2, sticky='we')
-    
+    table.grid(column=0, row=1, columnspan=2, sticky='nswe')
+
     root.mainloop()
