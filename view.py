fc9d65ca493a29a5a8adf45645f18e4a4b0d0394
evian-christ <thelastorcas@gmail.com>
2024-06-21 16:06:04 +0100

    setting default columns width

--- view.py
+++ view.py
@@ -15,6 +15,14 @@ def open_view():
                      ("Date", "Name", "Category", "Cost", "Rating", "Description", "Remark"),
                      show="headings")
 
+    table.column("Date", width=10)
+    table.column("Name", width=10)
+    table.column("Category", width=10)
+    table.column("Cost", width=10)
+    table.column("Rating", width=10)
+    table.column("Description", width=10)
+    table.column("Remark", width=10)
+
     table.heading("Date", text="Date")
     table.heading("Name", text="Name")
     table.heading("Category", text="Category")
@@ -25,8 +33,8 @@ def open_view():
 
     frame.grid(column=0, row=0, sticky="nsew")  # Expand frame within root
     root.columnconfigure(0, weight=1)
-    frame.columnconfigure(0, weight=1)  # Expand column 0 of root
-    frame.columnconfigure(1, weight=1)  # Expand column 0 of root
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(1, weight=1)
 
     advbtn.grid(column=1, row=0, sticky='e')
     table.grid(column=0, row=1, columnspan=2, sticky='nswe')
