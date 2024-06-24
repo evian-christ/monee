b219eef312d558450591813aec18f1e92cd5d777
evian-christ <thelastorcas@gmail.com>
2024-06-24 15:04:25 +0100

    edit button to pop up a new edit window (now just copied from add entry window)

--- view.py
+++ view.py
@@ -29,6 +29,62 @@ def delete_selected_row():
     # Remove from Treeview
     table.delete(item)
 
+def edit_selected_row():
+    selected_item = table.selection()
+    if not selected_item:
+        return
+    
+    root = Tk()
+
+    root.title("Edit " + str(selected_item[0]))
+    root.geometry("260x380+900+350")
+    root.resizable(0, 0)
+
+    frame = Frame(root)
+    
+    datelb = Label(frame, text = "Date: ", font=('Consolas', 10))
+    date = Entry(frame)
+    namelb = Label(frame, text = "Name: ", font=('Consolas', 10))
+    name = Entry(frame)
+    ctgrlb = Label(frame, text = "Category: ", font=('Consolas', 10))
+    ctgr = Combobox(
+        frame,
+        state="readonly",
+        values=["Food", "Entertainment", "Transport", "Misc"]
+    )
+    costlb = Label(frame, text = "Cost: ", font=('Consolas', 10))
+    cost = Entry(frame, width=6, justify=RIGHT)
+    ratelb = Label(frame, text = "Rating:", font=('Consolas', 10))
+    rate = Combobox(
+        frame,
+        width=2,
+        state="readonly",
+        values=[5,4,3,2,1]
+    )
+    desclb = Label(frame, text = "Desc: ", font=('Consolas', 10))
+    desc = Text(frame, height=5, width=10)
+    rmrklb = Label(frame, text = "Remark: ", font=('Consolas', 10))
+    rmrk = Text(frame, height=5, width=10)
+    addbtn = Button(frame, text="Add", command=on_submit)
+
+    frame.grid(column=0, row=0, sticky='n')
+
+    datelb.grid(column=0, row=0, padx=10, pady=(13, 10), sticky='w')
+    date.grid(column=1, row=0, columnspan=3, pady=(13, 10), sticky='we')
+    namelb.grid(column=0, row=1, padx=10, pady=10, sticky='w')
+    name.grid(column=1, row=1, columnspan=3, sticky='we')
+    ctgrlb.grid(column=0, row=2, padx=10, pady=10, sticky='w')
+    ctgr.grid(column=1, row=2, columnspan=3, sticky='we')
+    costlb.grid(column=0, row=3, padx=10, pady=10, sticky='w')
+    cost.grid(column=1, row=3, sticky='w')
+    ratelb.grid(column=2, row=3, padx=10, sticky='w')
+    rate.grid(column=3, row=3, sticky='w')
+    desclb.grid(column=0, row=4, padx=10, pady=10, sticky='nw')
+    desc.grid(column=1, row=4, columnspan=3, rowspan=2, pady=10, sticky='we')
+    rmrklb.grid(column=0, row=6, padx=10, pady=10, sticky='nw')
+    rmrk.grid(column=1, row=6, columnspan=3, rowspan=2, pady=10, sticky='we')
+    addbtn.grid(column=1, row=8, pady=5, columnspan=2, sticky='w')
+
 def open_view():
     global table
     root = Tk()
@@ -45,7 +101,7 @@ def open_view():
 
     advbtn = Button(frame, text="Advanced")
     delbtn = Button(frame, text="Delete", command=delete_selected_row)
-    editbtn = Button(frame, text="Edit")
+    editbtn = Button(frame, text="Edit", command=edit_selected_row)
     advbtn.grid(column=1, row=0, sticky='e')
     delbtn.grid(column=0, row=2, sticky='w')
     editbtn.grid(column=1, row=2, sticky='e')
