d4376b5fb4b73dce3e9e2784a026b8001708dc9a
evian-christ <thelastorcas@gmail.com>
2024-06-26 11:55:36 +0100

    changed the treeview to listbox and called the ctgrs from config file

--- settings.py
+++ settings.py
@@ -5,6 +5,23 @@ from dateAndTime import *
 import json
 import os
 
+#=====================================
+
+settings = {
+    "month_start_date": "1",
+    "category": ["Food", "Entertainment", "Transport", "Misc"],
+    "Budget": ["550", "150", "100", "100"]
+}
+
+if not os.path.exists('config.json'):
+    with open('config.json', 'w') as config_file:
+        json.dump(settings, config_file)
+else:
+    with open('config.json', 'r') as config_file:
+        settings = json.load(config_file)
+
+#=====================================
+
 def open_settings():
     root = Tk()
 
@@ -32,15 +49,18 @@ def open_settings():
     tab2 = Frame(notebook, padding=50)
     notebook.add(tab2, text="Category")
     categorylb = Label(tab2, text="Categories: ")
-    category = Treeview(tab2)
+    category = Listbox(tab2)
     category_edit = Button(tab2, text='edit')
     category_add = Button(tab2, text="+", width=5)
     category_del = Button(tab2, text="-", width=5)
-    categorylb.grid(column=0, row=0, sticky='w', pady=10)
-    category.grid(column=0, row=1, columnspan=3)
-    category_edit.grid(column=0, row=2, sticky='w')
+    categorylb.grid(column=0, row=0, sticky='w', pady=5)
+    category.grid(column=0, row=1, columnspan=3, sticky='nswe')
+    category_edit.grid(column=0, row=2, sticky='w', pady=5)
     category_add.grid(column=1, row=2, sticky='e')
     category_del.grid(column=2, row=2, sticky='e')
+
+    for i in settings['category']:
+        category.insert(END, i)
 
     #=====================================
 
