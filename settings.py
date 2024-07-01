376099ed627100c15a0762b19e806516d6761063
evian-christ <thelastorcas@gmail.com>
2024-07-01 11:23:23 +0100

    start date default fetched from config

--- settings.py
+++ settings.py
@@ -118,6 +118,8 @@ def open_settings():
         state="readonly",
         values=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
     )
+    startday.set(settings['month_start_date'])
+    print(startday.current())
     startdaylb.grid(column=0, row=0)
     startday.grid(column=1, row=0)
 
